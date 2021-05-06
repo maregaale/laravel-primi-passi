148ce734674390c89dda27389659e674a05c6329
Alessandro Marega <ale_marega@hotmail.it>
2021-05-06 15:05:19 +0200

    aggiunta rotta 'hello-world'

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello-world', function () {
+    return "Hello world!";
+});
+
+
+
+
